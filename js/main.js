ee565a36add7d1afe71f2f4f48e34824be603934
Rezmason <jeremysachs@rezmason.net>
2023-11-13 22:23:16 -0800

    Creating a scene, camera and full-page renderer with a resize handler and an animation loop

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
 import * as THREE from 'three';
+
+const container = document.body;
+
+const renderer = new THREE.WebGLRenderer( { antialias: true } );
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+container.appendChild( renderer.domElement );
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color( 0x000000 );
+
+const camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 100 );
+camera.position.set( 0, 0, 10 );
+
+window.onresize = () => {
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+};
+
+const animate = () => {
+	renderer.render( scene, camera );
+	requestAnimationFrame(animate);
+};
+
+animate();
